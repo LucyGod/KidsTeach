40575fb54b3cfa5f53861367d0f14efadca992bf
刘欣 <haloliuxin@gmail.com>
2019-11-12 10:34:04 +0800

    no message

--- KidTeach/Module/ASOContentViewController.m
+++ KidTeach/Module/ASOContentViewController.m
@@ -11,6 +11,7 @@
 
 @interface ASOContentViewController ()<DetailContentTopViewDelegate>{
     ASODetailContentTopView *_topCollectionView;
+    NSDictionary *_tempDataDic;
 }
 
 @end
@@ -21,25 +22,68 @@
     [super viewDidLoad];
     
     [self initSubViews];
+    
+    [self setDefaultValue];
+}
+
+- (void)setDefaultValue{
+    NSDictionary *dataDic = @{};
+    if ([_typeName isEqualToString:@"动物"]) {
+        dataDic = @{
+            @"name": @"鸡",
+            @"content" : @"家禽，品种很多，翅膀短，不能高飞；雄性啼能报晓，雄性生的蛋是好食品",
+            @"imageName": @"",
+            @"EnglishName": @"Chicken",
+            @"PinYin":@"jī"
+        };
+    }else if ([_typeName isEqualToString:@"水果"]) {
+        dataDic = @{
+            @"name": @"桃子",
+            @"content" : @"桃花可以观赏，果实多汁，可以生食，核仁可以使用。",
+            @"imageName": @"",
+            @"EnglishName": @"Peach",
+            @"PinYin":@"táo zi"
+        };
+    }else if ([_typeName isEqualToString:@"蔬菜"]) {
+        dataDic = @{
+            @"name": @"香菇",
+            @"content" : @"世界第二大食用菌，我国特产之一，素有”山珍“之称",
+            @"imageName": @"",
+            @"EnglishName": @"Mushroom",
+            @"PinYin":@"xiāng gū"
+        };
+    }else{
+        dataDic = @{
+            @"name": @"爸爸",
+            @"content" : @"爸爸的本领很大",
+            @"imageName": @"",
+            @"EnglishName": @"Father",
+            @"PinYin":@"bà ba"
+        };
+    }
+    
+    _tempDataDic = dataDic;
+    [self updateContentItems:dataDic];
 }
 
 - (void)initSubViews{
     self.contentImageView.layer.cornerRadius = 5.0;
-       self.contentImageView.layer.masksToBounds = YES;
-       
-       _topCollectionView = [[ASODetailContentTopView alloc] initWithFrame:CGRectZero contentType:self.typeName];
-       _topCollectionView.delegate = self;
-       [self.contentBGView addSubview:_topCollectionView];
-       [_topCollectionView mas_makeConstraints:^(MASConstraintMaker *make) {
-           make.left.equalTo(self.contentBGView).offset(20);
-           make.right.equalTo(self.contentBGView).offset(-20);
-           make.top.equalTo(self.contentBGView.mas_top).offset(45);
-           make.height.equalTo(@60);
-       }];
+    self.contentImageView.layer.masksToBounds = YES;
+    
+    _topCollectionView = [[ASODetailContentTopView alloc] initWithFrame:CGRectZero contentType:self.typeName];
+    _topCollectionView.delegate = self;
+    [self.contentBGView addSubview:_topCollectionView];
+    [_topCollectionView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.contentBGView).offset(20);
+        make.right.equalTo(self.contentBGView).offset(-20);
+        make.top.equalTo(self.contentBGView.mas_top).offset(45);
+        make.height.equalTo(@60);
+    }];
 }
 
 - (void)didSelectdTopViewItemAtIndexpath:(NSIndexPath *)indexpath param:(NSDictionary *)paramDic{
     NSLog(@"%ld____%@",indexpath.row,paramDic);
+    _tempDataDic = paramDic;
     [self updateContentItems:paramDic];
 }
 
@@ -57,12 +101,15 @@
 }
 
 - (IBAction)voiceButtonAction:(id)sender {
-    
+    //当前json dic
+//    _tempDataDic[@"name"];
 }
 
 - (IBAction)tapNextButtonAction:(id)sender {
 }
 
 - (IBAction)contentVoiceButtonAction:(id)sender {
+       //当前json dic
+    //    _tempDataDic[@"content"];
 }
 @end
