3b7307c876aa86660bf58fcb5661341e445a8767
MAC_DH <mac@MACdeMBP.lan>
2019-11-15 18:32:57 +0800

    Wifi

--- LDDVideoPro/Module/Second/SecondViewController.m
+++ LDDVideoPro/Module/Second/SecondViewController.m
@@ -6,8 +6,16 @@
 //
 
 #import "SecondViewController.h"
+#import "HmHTTPConnection.h"
+#import "HmTool.h"
+#import <HTTPServer.h>
 
-@interface SecondViewController ()
+@interface SecondViewController (){
+    HTTPServer *httpServer;
+}
+
+@property (nonatomic, strong) UILabel *urlLabel;
+
 @end
 
 @implementation SecondViewController
@@ -21,9 +29,78 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-
+    UIView *bgView = [[UIView alloc] init];
+    bgView.backgroundColor = [UIColor colorWithHexString:@"5fa6f8"];
+    [self.view addSubview:bgView];
+    [bgView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.top.equalTo(self.view);
+        make.height.mas_equalTo(320);
+    }];
+    
+    UILabel *message = [[UILabel alloc] init];
+    message.numberOfLines = 2;
+    message.textAlignment = NSTextAlignmentCenter;
+    message.text = @"确保您的iPhone和PC在同一局域网，文件\n上传过程中请勿退出";
+    message.textColor = [UIColor whiteColor];
+    [bgView addSubview:message];
+    [message mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(bgView);
+        make.height.mas_equalTo(60);
+        make.bottom.equalTo(bgView.mas_bottom).offset(-60);
+    }];
+    
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.numberOfLines = 2;
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.text = @"在PC的浏览器中访问以下地址";
+    titleLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:titleLabel];
+    [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.view);
+        make.height.mas_equalTo(20);
+        make.top.equalTo(bgView.mas_bottom).offset(40);
+    }];
+    
+    [self.view addSubview:self.urlLabel];
+    [self.urlLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view.mas_centerX);
+        make.width.mas_equalTo(280);
+        make.height.mas_equalTo(44);
+        make.top.equalTo(titleLabel.mas_bottom).offset(20);
+    }];
+    
+    [self initServer];
 
 }
 
+// 初始化本地服务器
+- (void)initServer {
+    httpServer = [[HTTPServer alloc] init];
+    [httpServer setType:@"_http._tcp."];
+    // webPath是server搜寻HTML等文件的路径
+    NSString *webPath = [[NSBundle mainBundle] resourcePath];
+    [httpServer setDocumentRoot:webPath];
+    [httpServer setConnectionClass:[HmHTTPConnection class]];
+    NSError *error;
+    if ([httpServer start:&error]) {
+        self.urlLabel.text = [NSString stringWithFormat:@"http://%@:%hu",[HmTool getIPAddress:YES],[httpServer listeningPort]];
+        NSLog(@"IP: %@:%hu", [HmTool getIPAddress:YES], [httpServer listeningPort]);
+    }else {
+        NSLog(@"%@", error);
+    }
+}
+
+- (UILabel *)urlLabel {
+    if (!_urlLabel) {
+        _urlLabel = [[UILabel alloc] init];
+        _urlLabel.backgroundColor = [UIColor colorWithHexString:@"5fa6f8"];
+        _urlLabel.layer.cornerRadius = 22;
+        _urlLabel.layer.masksToBounds = YES;
+        _urlLabel.textAlignment = NSTextAlignmentCenter;
+        _urlLabel.text = @"";
+        _urlLabel.textColor = [UIColor whiteColor];
+    }
+    return _urlLabel;
+}
 
 @end
--- LDDVideoPro/Module/Third/ThirdViewController.m
+++ LDDVideoPro/Module/Third/ThirdViewController.m
@@ -8,16 +8,74 @@
 #import "ThirdViewController.h"
 
 
-@interface ThirdViewController ()
+@interface ThirdViewController ()<UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tableV;
+@property (nonatomic, strong) NSMutableArray *dirArray; // 存储沙盒里面的所有文件
 @end
 
 @implementation ThirdViewController
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self setupView];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setupTableView];
+    [self setupView];
+}
 
+- (void)setupView {
+    
+    
+    // 获取沙盒中所有文件
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    // 在这里获取应用程序Documents文件夹里的文件及文件夹列表
+    NSArray *documentPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentDir = [documentPaths objectAtIndex:0];
+    NSError *error = nil;
+    NSArray *fileList = [[NSArray alloc] init];
+    // fileList便是包含有该文件夹下所有文件的文件名及文件夹名的数组
+    fileList = [fileManager contentsOfDirectoryAtPath:documentDir error:&error];
+    
+    self.dirArray = [[NSMutableArray alloc] init];
+    for (NSString *fileName in fileList) {
+        [self.dirArray addObject:fileName];
+    }
+    [self.tableV reloadData];
+}
 
+- (void)setupTableView {
+    self.tableV = [[UITableView alloc] initWithFrame:self.view.bounds style:(UITableViewStylePlain)];
+    self.tableV.backgroundColor = [UIColor lightGrayColor];
+    self.tableV.tableFooterView = [UIView new];
+    self.tableV.delegate = self;
+    self.tableV.dataSource = self;
+    self.tableV.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.tableV.rowHeight = UITableViewAutomaticDimension;
+    self.tableV.estimatedRowHeight = 100;
+    [self.view addSubview:_tableV];
+}
+
+#pragma mark -- UITableView Delegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dirArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifier = @"reuseIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:(UITableViewCellStyleDefault) reuseIdentifier:identifier];
+    }
+    cell.textLabel.text = self.dirArray[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
